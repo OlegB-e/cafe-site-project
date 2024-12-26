8b936dc9fabeeeb7ff3732f8289d28a76441d843
OlegB-e < Email,   GitHub>
2024-12-27 01:45:08 +0200

    Добавлена главная страница с фоновым изображением

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
-// src/pages/Home.jsx
 import React from 'react';
+import './Home.css';  // Импортируем файл стилей для главной страницы
 
 const Home = () => {
-  return <h1>Добро пожаловать на Главную страницу!</h1>;
+  return (
+    <div className="home-container">
+      <div className="content">
+        <h1 className="title">Добро пожаловать в кафе "Золотая бузза"</h1>
+        <p className="description">Вкусные блюда и освежающие напитки ждут вас!</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
